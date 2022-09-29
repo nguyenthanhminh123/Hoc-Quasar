42be3a95b866bd27e086dcf061adb9ffbd7c7478
thanhminh <you@example.com>
2022-09-29 10:57:32 +0700

    Lesson-05

---
+++ Quasar-App5/Functionalities Capabilities/src/pages/IndexPage.vue
@@ -0,0 +1,55 @@
+<template>
+  <q-page class="flex flex-center">
+    <div>
+      <q-table
+        dense
+        separator="none"
+        title="People"
+        :rows="rows"
+        :colomns="columns"
+        row-kay="firstName"
+      />  
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'IndexPage',
+  data() {
+    return {
+      alert: false,
+      maximized: true,
+      tab: 'one',
+      isLoading: true,
+      useData: false,
+      useWifi: false,
+      columns: [
+        {
+          name: 'name',
+          label: 'First Name',
+          field: 'name'
+        },
+        {
+          name: 'lastname',
+          label: 'Last Name',
+          field: 'lastname'
+
+        }
+      ],
+      rows: [
+        {
+          name: 'Minh',
+          lastName: 'Jadia',
+        },
+        {
+          name: 'Minquan',
+          lastName: 'Jia',
+        }
+      ]
+    }
+  }
+})
+</script>
