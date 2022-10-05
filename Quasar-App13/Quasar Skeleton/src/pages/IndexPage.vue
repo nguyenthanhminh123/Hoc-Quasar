d0a42604123b7319f2c39348c481d9534817d139
thanhminh <you@example.com>
2022-10-05 15:02:59 +0700

    Lesson-13

---
+++ Quasar-App13/Quasar Skeleton/src/pages/IndexPage.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-page >
+    <div class="q-pa-md">
+      <div style="max-width:300px">
+        <q-from @submit="submitForm">
+          <div>
+            <q-input outlined rounded clearable clear-icon="close" color="teal" label="Your Name" v-model="formData.textInput" 
+            :rules="[val => !!val || 'Field is required']" 
+            />
+          </div>
+          <div class="q-pt-md">
+            <q-btn type="submit" label="Submit"/>
+          </div>
+        </q-from>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'IndexPage',
+  data() {
+    return {
+     formData: {
+      textInput: null
+     }
+    }
+  },
+  methods: {
+    submitForm() {
+      alert('You name is:' + this,formData.textInput);
+    }
+  }
+})
+</script>
